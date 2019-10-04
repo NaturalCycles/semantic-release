9b01fb54a69607268595be240fc389555a83e826
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-10-04 20:33:07 +0200

    feat: support --config

--- src/bin/release.js
+++ src/bin/release.js
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
 
 const semanticRelease = require('semantic-release')
-const config = require('../../cfg/release.config')
+const yargs = require('yargs')
+const path = require('path')
 
-semanticRelease(config).catch(err => {
+main().catch(err => {
   console.error(err)
   process.exit(1)
 })
+
+async function main() {
+  const {config: configPath} = yargs.options({
+    config: {
+      type: 'string',
+      descr: 'Path to config file',
+    }
+  }).argv
+
+  const cwd = process.cwd()
+  const config = configPath
+    ? require(path.resolve(cwd, configPath))
+    : require('../../cfg/release.config') // defaultConfig
+  // console.log(config) // debug
+  await semanticRelease(config)
+}
