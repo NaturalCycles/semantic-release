3b59c6b6def31756c1fb40dab0e1aac910e1ef3e
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-10-26 00:55:56 +0200

    feat: stop generating changelog, use github releases instead
    BREAKING CHANGE: ^^^

--- cfg/release.config.js
+++ cfg/release.config.js
@@ -9,11 +9,13 @@ module.exports = {
     {name: 'beta', prerelease: true},
     {name: 'alpha', prerelease: true}
   ],
+  // Default list of plugins is in `get-config.js`
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    // '@semantic-release/changelog', // let's skip changelog, as github releases can be used instead
     '@semantic-release/npm',
-    '@semantic-release/git',
+    // '@semantic-release/git', // let's use github instead
+    '@semantic-release/github',
   ],
 }
